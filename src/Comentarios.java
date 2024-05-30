749f4bd75019edc56b139a90c56437d6a837abfc
Sofia Prata <158978435+sofiaprata@users.noreply.github.com>
2024-05-29 22:22:14 -0300

    documentação de java (comentarios e tags)

---
+++ src/Comentarios.java
@@ -0,0 +1,14 @@
+public class Comentarios {
+    public static void main(String[] args){
+        // Olá, eu sou um comentário em uma só linha
+
+        /*Olá
+         * Eu sou um comentario
+         * que posso ser mais detalhado
+         * quando necessario
+         */
+    }
+    public void metodo(){
+
+    }
+}
